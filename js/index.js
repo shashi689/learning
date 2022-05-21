c514eb5279b2470361c1fee73c5ad8b72eab91b2
shashi689 <shashi.76k@gmail.com>
2022-05-21 17:28:10 +0530

    added

---
+++ js/index.js
@@ -0,0 +1,68 @@
+//  var number1= 10;
+// var number2= 20;
+// console.log( number1 + number2);
+
+// Array
+// var arr = [1, 2, 3, 4, 5];
+// console.log(arr);        
+// console.log(arr[0]);        
+// console.log(arr[4]);
+
+// Strings added
+// var arr = [1, "another number", 3, 4, 5];
+// console.log(arr);        
+// console.log(arr[1]);        
+// console.log(arr[4]);  
+
+// logical operators
+
+// cosole.log( true && true);
+
+// function avgheat(a, b, c, d) {
+//     e =(a+b+c+d)/4;
+// return e;
+// }
+
+// day1= avgheat(10, 9, 10, 11);
+// day2= avgheat(2, 9, 10, 11);
+// console.log(day9, 9, 10, 8);
+// day3= avgheat(11);
+// console.log(day2);
+// console.log(day3);
+
+// var a= 10;
+// var b= 100;
+// console.log( a, b);
+// console.log( a + b);
+// a = 'i am a now';
+// b = ' i am b now';
+// console.log(a, b);
+// console.log(a + b);
+// const c = 'i am string 1 ' + 'i am sting 2';
+// const d = 'c';
+// console.log(c);
+// console.log(d);
+
+// const a = 0.15;
+// const b = 0.001;
+// console.log(a, b);
+// console.log(a + b);
+// const pen = 1;
+// const paper = 2;
+// console.log(pen + paper);
+// console.log(pen - paper);
+// const pen =paper;
+// console.log(pen);
+// const 1= 5;
+// const 2 = 10;
+// console.log( 1, 2);
+// const book= 'read';
+// const page ='book';
+// console.log( page , a , b);
+// console.log(a*b);
+// console.log(a/b);
+
+// alert("Hello World");
+// console.log("Hello World");
+
+
