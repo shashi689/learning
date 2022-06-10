7a930be7e1f71f51cc886df13fb3310682b6b385
shashi689 <shashi.76k@gmail.com>
2022-06-10 19:03:27 +0530

    js practice

---
+++ Youtube/index.js
@@ -0,0 +1,63 @@
+function areaOfCircle(r){
+    return 3.14* (r * r);
+}
+// console.log(areaOfCircle(8));
+
+function  areaOfSquare(s){
+    return s * s;
+}
+// console.log(areaOfSquare(8));
+
+function simpleInterest(p, r, t){
+    return (p* r * t)/ 100;
+}
+// console.log(simpleInterest(2000, 10, 5));
+
+function volumeOfCylider(r, h){
+    return 3.14 * (r * r) * h;
+}
+// console.log(volumeOfCylider(10, 3))
+
+const arr= ['ram', 'shyam', 10, 'ravi'];
+// arr[0]= 'raj';
+// arr.push= 'rohan';
+// console.log(arr[0]);
+// console.log(arr);
+// console.log(arr.length);
+
+
+
+
+
+    function inputYear(y){
+        if (y % 4 === 0){
+            return true;
+        }
+        return false;
+    }
+    // console.log(inputYear('2030'));
+
+    function enterRollNumber(n){
+        if (n === '1'){
+            return 'Abhijit';
+        }
+        else if (n === '2'){
+            return 'Abhishek';
+        }
+        else if (n === '3'){
+            return 'Amit';
+        }
+    }
+    // console.log(enterRollNumber('3'));
+        
+    // function largestNumber(n){
+    //     let i = n[0];
+    //     if()
+
+
+        
+    //     }
+    
+    // console.log(largestNumber([1, 2, 3, 4]));
+
+    
