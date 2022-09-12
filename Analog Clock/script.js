cf0b963cf114502bef03c8ff22db292f1481afab
shashi689 <shashi.76k@gmail.com>
2022-09-12 19:57:59 +0530

    complete

--- Analog Clock/script.js
+++ Analog Clock/script.js
@@ -8,5 +8,9 @@ setInterval(() => {
     mrotation = 6*mtime;
     srotation = 6*stime;
 
-    
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+
 }, 1000);
