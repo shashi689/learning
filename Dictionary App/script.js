9334a6520ea519162d5e4950f22bd39820481e6b
shashi689 <shashi.76k@gmail.com>
2022-11-05 16:35:14 +0530

    dictionary app

--- Dictionary App/script.js
+++ Dictionary App/script.js
@@ -1,31 +1,55 @@
-const resultDiv = document.querySelector('.result');
-const wordEle = document.querySelector('#word');
-const phonetics = document.querySelector('.phonetics');
-const audio = document.querySelector('audio');
-const wordMeaning = document.querySelector('.word-definition');
-const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'; 
+const resultDiv = document.querySelector(".result");
+const wordEle = document.querySelector("#word");
+const phonetics = document.querySelector(".phonetics");
+const audio = document.querySelector("audio");
+const wordMeaning = document.querySelector(".word-definition");
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
-
-const handle = async (e) =>{
-    if(e.keyCode == 13){
-        const word = e.target.value;
-        // make a request to api
-        const result = await fetch(url + word);
-        const data = await result.json();
-        resultDiv.style.display = 'block';
-
-        wordEle.innerText = data[0].word;
-        phonetics.innerText = data[0].phonetics[0].text;
-        audio.src = data[0].phonetics[0].audio;
-        wordMeaning.innerText = data[0].meanings[0].definitions[0].definition;
-        const synonyms = data[0].meanings[0].definitions[0].synonyms;
-        let synonymsData = '';
-        for(let i=0; i < synonyms.length; i++){
-            synonymsData += `<p class= 'pills'>${synonyms[i]}</p>`;
+const handle = async (e) => {
+  if (e.keyCode == 13) {
+    const word = e.target.value;
+    // make a request to api
+    const result = await fetch(url + word);
+    resultDiv.style.display = "block";
+    const data = await result.json();
+    
+    if (result.ok) {
+      document.querySelectorAll('.wordmeaning')[0].style.removeProperty('display');
+      document.querySelectorAll('.wordmeaning')[1].style.removeProperty('display');
+      phonetics.style.removeProperty('display');
+      audio.style.removeProperty('display');
+      // const data = await result.json();
+      // resultDiv.style.display = "block";
+      // console.log(resultDiv);
+      wordEle.innerText = data[0].word;
+      phonetics.innerText = data[0].phonetic;
+      audio.src = data[0].phonetics[0].audio;
+      wordMeaning.innerText = data[0].meanings[0].definitions[0].definition;
+      const synonymsArray = data[0].meanings[0].definitions[0].synonyms;
+      let synonymsData = "";
+      if (synonymsArray.length) {
+        for (let i = 0; i < synonymsArray.length; i++) {
+          synonymsData += `<a class='anchor' href='#'>${synonymsArray[i]}</a>`;
         }
-        document.querySelector('.synonyms').innerHTML = synonymsData;
+      }
+      else{
+        synonymsData = `<a class='anchor'>Hai Nai kuch</a>`;
+      }
+      document.querySelector(".synonyms").innerHTML = synonymsData;
+    }
+    else{
+      audio.style.display = 'none';
+      wordEle.innerText = data.title;
+      phonetics.style.display= 'none';
+      document.querySelectorAll('.wordmeaning')[0].style.display= 'none';
+      document.querySelectorAll('.wordmeaning')[1].style.display= 'none';
+      wordMeaning.innerText= data.message;
+      document.querySelector('.synonyms').style.display= 'none';
+    }
+  }
+};
 
-        }
+ myFunction = (word)=>{
+  const otherWord = word.textContent;
 }
-
 
